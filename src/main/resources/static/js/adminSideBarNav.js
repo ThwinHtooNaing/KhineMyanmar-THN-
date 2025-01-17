31c46e91a50c3dfa8d6ed4dca9693aac8b9e5a98
Noah Thwin <sthit312@gmail.com>
2025-01-18 01:04:07 +0630

    With Pagaing in user table

--- src/main/resources/static/js/adminSideBarNav.js
+++ src/main/resources/static/js/adminSideBarNav.js
@@ -8,6 +8,7 @@ const sidebarDivs = document.querySelectorAll('.sidebar div');
         // Add click event listener to each div
         sidebarDivs.forEach(div => {
             div.addEventListener('click', () => {
+                event.preventDefault();
                 const path = div.getAttribute('data-path'); // Get the path from the data attribute
                 if (path) {
                     window.location.href = path; // Navigate to the path
