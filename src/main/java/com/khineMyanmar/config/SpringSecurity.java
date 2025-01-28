1d765c497369ee908233dff9fa66338f49395b5c
Noah Thwin <sthit312@gmail.com>
2025-01-28 12:55:39 +0630

    `Updated SpringSecurity.java, adminIndex.html, shopOwnerIndex.html and shopOwnerOrders.html with minor changes.`

--- src/main/java/com/khineMyanmar/config/SpringSecurity.java
+++ src/main/java/com/khineMyanmar/config/SpringSecurity.java
@@ -1,4 +1,4 @@
-	package com.khineMyanmar.config;
+package com.khineMyanmar.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -12,8 +12,6 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
-import com.khineMyanmar.model.User;
-
 @Configuration
 @EnableWebSecurity
 public class SpringSecurity {
